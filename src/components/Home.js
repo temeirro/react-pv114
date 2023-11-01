7148c13eacdd9f667132d9efb52b2da263fa9736
TSShrol <shrolts@gmail.com>
2023-11-01 22:55:21 +0200

    Satart createTimer in Home Component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,26 @@
+import { useEffect, useState } from "react";
+
 export default function Home() {
+    //init state current time
+    const [currentTime, setCurrentTime]=useState(new Date().toLocaleTimeString());
+    const timerChange=()=>{
+        setCurrentTime(new Date().toLocaleTimeString())
+    };
+
+    useEffect(() => {
+        console.log("Effect invoke!!!!");
+        setInterval(timerChange,1000);
+    },[]);
+
     return (
         <>
             <div className="textInfo">
                 <h2>Welcam home</h2>
             </div>
-          
+            <div>
+                {/* <h3>{new Date().toLocaleTimeString()}</h3> */}
+                <h3>{currentTime}</h3>
+            </div>
         </>
     );
 }
